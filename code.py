6aec42daee23a07b1aed06e12b82eade97e36f71
David Witt <david@davidwitt.com>
2021-05-01 18:28:25 -0400

    CAN send working

--- code.py
+++ code.py
@@ -1,4 +1,5 @@
 import time
+import random
 import struct
 
 import board
@@ -22,7 +23,7 @@ from adafruit_display_text import label
 #from adafruit_bitmap_font import bitmap_font
 
 
-from HoneywellHSC import HoneywellHSC
+from honeywellHSC import HoneywellHSC 
 from kalman_filter import KalmanFilter
 
 # Constants
@@ -31,16 +32,24 @@ OUTPUT_MAX = 14745
 PRESSURE_MIN = 0
 PRESSURE_MAX = 103421
 
-QNH = 2992
-previous_QNH = QNH
+qnh = 2992
+previous_qnh = qnh
 
 # ----------------------------------------------------------------------
 # --- CAN Message Numbers                                            ---
 # ----------------------------------------------------------------------
 
-AAV_id = 0x028
-STATICP_id = 0x02B
-QNH_id = 0x02E
+CAN_Air_Msg_id = 0x028
+CAN_Raw_Msg_id = 0x02B
+CAN_QNH_Msg_id = 0x02E
+
+CAN_Air_Period = 200 # ms between messages
+CAN_Raw_Period = 500 # ms between messages
+CAN_QNH_Period = 5000 # ms between messages
+
+CAN_Air_Timestamp = 0
+CAN_RAW_Timestamp = 0
+CAN_QNH_Timestamp = 0
 
 
 
@@ -64,10 +73,12 @@ i2c = board.I2C()
 if hasattr(board, 'CAN_STANDBY'):
     standby = digitalio.DigitalInOut(board.CAN_STANDBY)
     standby.switch_to_output(False)
+    print("Standby off")
     
 if hasattr(board, 'BOOST_ENABLE'):
     boost_enable = digitalio.DigitalInOut(board.BOOST_ENABLE)
     boost_enable.switch_to_output(True)
+    print("Boost On")
     
 can = canio.CAN(rx=board.CAN_RX, tx=board.CAN_TX,
                 baudrate=250_000, auto_restart=True)
@@ -126,7 +137,7 @@ PRES_text_area.text = str(101325)
 QNH_text_area = label.Label(font = font, text = text, color = color)
 QNH_text_area.anchor_point = (1.0, 0)
 QNH_text_area.anchored_position = (128,0)
-QNH_text_area.text = str(QNH / 100)
+QNH_text_area.text = str(qnh / 100)
 
 group.append(ALT_text_area)
 group.append(ALT_LABEL_text_area)
@@ -149,14 +160,13 @@ last_Time_Millis = int(time.monotonic_ns() / 1000000)
 #     text_area.text ="help3"
 #    pass
 
-# ----------------------------------------------------------------------
-# --- Create a CAN bus message mask for the QNH message              ---
-# ----------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# --- Create a CAN bus message mask for the QNH message and create a        ---
+# --- listener                                                              ---
+# -----------------------------------------------------------------------------
 
-qnh_match = canio.Match(id=QNH_id)
+qnh_match = canio.Match(id=CAN_QNH_Msg_id)
 qnh_listener = can.listen(matches=[qnh_match], timeout=0.1)
-
-
 
 # ------------------------------------------------------------------------------
 # Wait here until we can lock the i2C to allow us to scan it
@@ -173,17 +183,21 @@ try:
 
     print("I2C address found:", [hex(device_address) for device_address
                                  in i2c.scan()])
-
     i2c.unlock()
-# ------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 #try:
 
-    # Get the pressure
+    # --- Read the initial presser from the transducer                      ---
     my_hsc.read_transducer()
     
     static_pressure = my_hsc.pressure
     static_pressure = pressure_filter.filter(static_pressure)
+    
+    altitude = int(145442.0 * (
+        pow(float(qnh / 2992), 0.1902632)
+        - pow(float(static_pressure / 101325), 0.1902632)))
 
+    # --- Set the previous values to 0 to trigger future operations         ---
     previous_static_pressure = 0
     previous_altitude = 0
 
@@ -206,13 +220,13 @@ try:
  
     while True:
 
-        current_Time_Millis = int(time.monotonic_ns() / 1000000)
+        current_time_millis = int(time.monotonic_ns() / 1000000)
 
 #       --- Read the pressure transducer after every 50 ms           ---
 
-        if current_Time_Millis - last_Time_Millis > 50:
+        if current_time_millis - last_Time_Millis > 50:
             
-            last_Time_Millis = current_Time_Millis
+            last_Time_Millis = current_time_millis
             
             my_hsc.read_transducer()
             
@@ -220,10 +234,12 @@ try:
             static_pressure = my_hsc.pressure
             static_pressure = pressure_filter.filter(static_pressure)
 
+            if (static_pressure != previous_static_pressure):
+                PRES_text_area.text = str(int(static_pressure))
 
-            PRES_text_area.text = str(int(static_pressure))
-
-        # --- Check for a QNH message on the CAN bus                 ---
+        # ---------------------------------------------------------------------
+        # --- Check for a QNH message on the CAN bus                        ---
+        # ---------------------------------------------------------------------
         
         if (qnh_listener.in_waiting):
             message = qnh_listener.receive
@@ -239,50 +255,88 @@ try:
                 # QNH contains a two byte short integer
                 qnh_hpa = data.unpack("<h")
                 # convert hPa to inHg * 100
-                previous_QNH = QNH
-                QNH = int(qnh_hpa * 0.0295299875 * 100)
+                previous_qnh = qnh
+                qnh = int(qnh_hpa * 0.0295299875 * 100)
         
-        # --- QNH updated if recieved                                ---
+        # --- qnh updated if recieved                                ---
 
         # --- update the display only if the value changed    
 
-        if (previous_QNH != QNH):
-            QNH_text_area.text = str(QNH / 100)
+        if (previous_qnh != qnh):
+            QNH_text_area.text = str(qnh / 100)
 
-        # --- Calculate the altitued only if the pressure or QNH     ---
-        # --- changed                                                ---
+        # ---------------------------------------------------------------------
+        # --- Calculate the altitued only if the pressure or QNH            ---
+        # --- changed                                                       ---
+        # ---------------------------------------------------------------------
 
         if (previous_static_pressure != static_pressure or 
-            previous_QNH != QNH):
+            previous_qnh != qnh):
 
+            previous_altitude = altitude
             altitude = int(145442.0 * (
-                pow(float(QNH / 2992), 0.1902632)
+                pow(float(qnh / 2992), 0.1902632)
                 - pow(float(static_pressure / 101325), 0.1902632)
             ))
 
         my_hsc.read_transducer()
         temperature = my_hsc.temperature
 
+        # ---------------------------------------------------------------------
+        # --- Display the new altitude if changed                           ---
+        # ---------------------------------------------------------------------
+        
         if (altitude != previous_altitude):
             ALT_text_area.text = str(altitude)
-            previous_altitude = altitude
 
-        # --- Send CAN data
-        # --- Initially just send this periodically                  ---
 
-        AAV_data = struct.pack("<hBBBhB",
-                               0,
-                               altitude & 0x0ff,
-                               (altitude >> 8) & 0xff,
-                               (altitude >> 16) & 0xff,
-                               0,
-                               0)
-        STATICP_data = struct.pack("<hbBBBBB",
-                                   int(static_pressure / 10),
-                                   int(temperature),
-                                   0,0,0,0,0)
+        # ---------------------------------------------------------------------
+        # --- Send CAN data                                                 ---
+        # ---------------------------------------------------------------------
 
-        print(STATICP_data)
+        # --- Send Air Speed, Altitude and Vertical Speed                   ---
+        if (current_time_millis > CAN_Air_Timestamp + CAN_Air_Period + 
+            random.randint(0,50)):
+            
+            Air_data = struct.pack("<hBBBhB",
+                                   0,
+                                   altitude & 0x0ff,
+                                   (altitude >> 8) & 0xff,
+                                   (altitude >> 16) & 0xff,
+                                   0,
+                                   0)
+            message = canio.Message(CAN_Air_Msg_id, Air_data)
+            can.send(message)
+            CAN_Air_Timestamp = current_time_millis
+            print("Can Air")
+          
+        # --- Send Raw pressure and temperature sensor data                 ---  
+        if (current_time_millis > CAN_RAW_Timestamp + CAN_Raw_Period +
+            random.randint(0,50)):
+            
+            Raw_data = struct.pack("<hbBBBBB",
+                                int(static_pressure / 10),
+                                int(temperature),
+                                0,0,0,0,0)
+            message = canio.Message(CAN_Raw_Msg_id, Raw_data)
+            can.send(message)
+            CAN_RAW_Timestamp = current_time_millis
+            print("Can Raw")
+            
+        # --- Send the qnh value if it has not been updated. qnh is         ---
+        # --- normally sent from the display (rPi) but if not received is   ---
+        # --- rebroadcast in case something else needs it.                  ---
+        if (current_time_millis > CAN_QNH_Timestamp + CAN_QNH_Period):
+            qnh_hpa = int(qnh / 2.95299875)
+            QNH_data = struct.pack("<hBBBBBB",
+                                   qnh_hpa,
+                                   0,0,0,0,0,0)
+            message = canio.Message(CAN_QNH_Msg_id, QNH_data)
+            can.send(message)
+            CAN_QNH_Timestamp = current_time_millis
+            print("Can QNH")
+
+        
 finally:
     print("Unlocking")
     i2c.unlock()
