99b9274ff1ca6899f9ad8809d368f7b2fd02c26e
David Witt <david@davidwitt.com>
2025-01-11 11:51:46 -0500

    Updating files to work with pixi.js v8

--- Python_Avionics/copytopiefis.sh
+++ Python_Avionics/copytopiefis.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# Script to be run from the RPi4 directory
+# Script to be run from the Python_Avionics directory
 
+# remote directories on the pi-efis computer
 piefis_main_dir=/home/david/Python_Avionics/
 piefis_support_dir=/home/david/Python_Avionics/support/
 piefis_linux_dir=/home/david/Python_Avionics/linux/
@@ -18,33 +19,38 @@ rsync aio_server.py "$user"@"$destination_server":"$piefis_main_dir"aio_server.p
 
 rsync support/efis-display.js "$user"@"$destination_server":"$piefis_support_dir"efis-display.js
 rsync support/ribbon.mjs "$user"@"$destination_server":"$piefis_support_dir"ribbon.mjs
-rsync support/airspeedRibbon.mjs "$user"@"$destination_server":"$piefis_support_dir"airspeedRibbon.mjs
-rsync support/vsi-indicator.mjs "$user"@"$destination_server":"$piefis_support_dir"vsi-indicator.mjs
+# rsync support/airspeedRibbon.mjs "$user"@"$destination_server":"$piefis_support_dir"airspeedRibbon.mjs
+# rsync support/vsi-indicator.mjs "$user"@"$destination_server":"$piefis_support_dir"vsi-indicator.mjs
 rsync support/attitude-indicator.mjs "$user"@"$destination_server":"$piefis_support_dir"attitude-indicator.mjs
-rsync support/slipBall.mjs "$user"@"$destination_server":"$piefis_support_dir"slipBall.mjs
-rsync support/headingIndicator.mjs "$user"@"$destination_server":"$piefis_support_dir"headingIndicator.mjs
-rsync support/interaction.mjs "$user"@"$destination_server":"$piefis_support_dir"interaction.mjs
-rsync support/specialRectangle.mjs "$user"@"$destination_server":"$piefis_support_dir"specialRectangle.mjs
-rsync support/qnhdisplay.mjs "$user"@"$destination_server":"$piefis_support_dir"qnhdisplay.mjs
-rsync support/speedDisplay.mjs "$user"@"$destination_server":"$piefis_support_dir"speedDisplay.mjs
-rsync support/altitudeDisplay.mjs "$user"@"$destination_server":"$piefis_support_dir"altitudeDisplay.mjs
-rsync support/userInput.mjs "$user"@"$destination_server":"$piefis_support_dir"userInput.mjs
-rsync support/numericWheelDisplay.mjs "$user"@"$destination_server":"$piefis_support_dir"numericWheelDisplay.mjs
-rsync support/utilityFunctions.mjs "$user"@"$destination_server":"$piefis_support_dir"utilityFunctions.mjs
-rsync support/tempTimeDisplay.mjs "$user"@"$destination_server":"$piefis_support_dir"tempTimeDisplay.mjs
-rsync support/airSpeedWheel.mjs "$user"@"$destination_server":"$piefis_support_dir"airSpeedWheel.mjs
-rsync support/altitudeWheel.mjs "$user"@"$destination_server":"$piefis_support_dir"altitudeWheel.mjs
-rsync support/brightness.mjs "$user"@"$destination_server":"$piefis_support_dir"brightness.mjs
-rsync support/temperatureGraph.mjs "$user"@"$destination_server":"$piefis_support_dir"temperatureGraph.mjs
+# rsync support/slipBall.mjs "$user"@"$destination_server":"$piefis_support_dir"slipBall.mjs
+# rsync support/headingIndicator.mjs "$user"@"$destination_server":"$piefis_support_dir"headingIndicator.mjs
+# rsync support/interaction.mjs "$user"@"$destination_server":"$piefis_support_dir"interaction.mjs
+# rsync support/specialRectangle.mjs "$user"@"$destination_server":"$piefis_support_dir"specialRectangle.mjs
+# rsync support/qnhdisplay.mjs "$user"@"$destination_server":"$piefis_support_dir"qnhdisplay.mjs
+# rsync support/speedDisplay.mjs "$user"@"$destination_server":"$piefis_support_dir"speedDisplay.mjs
+# rsync support/altitudeDisplay.mjs "$user"@"$destination_server":"$piefis_support_dir"altitudeDisplay.mjs
+# rsync support/userInput.mjs "$user"@"$destination_server":"$piefis_support_dir"userInput.mjs
+# rsync support/numericWheelDisplay.mjs "$user"@"$destination_server":"$piefis_support_dir"numericWheelDisplay.mjs
+# rsync support/utilityFunctions.mjs "$user"@"$destination_server":"$piefis_support_dir"utilityFunctions.mjs
+# rsync support/tempTimeDisplay.mjs "$user"@"$destination_server":"$piefis_support_dir"tempTimeDisplay.mjs
+# rsync support/airSpeedWheel.mjs "$user"@"$destination_server":"$piefis_support_dir"airSpeedWheel.mjs
+# rsync support/altitudeWheel.mjs "$user"@"$destination_server":"$piefis_support_dir"altitudeWheel.mjs
+# rsync support/brightness.mjs "$user"@"$destination_server":"$piefis_support_dir"brightness.mjs
+# rsync support/temperatureGraph.mjs "$user"@"$destination_server":"$piefis_support_dir"temperatureGraph.mjs
 
 #rsync support/pixi.min-v8.1.5.mjs "$user"@"$destination_server":"$piefis_support_dir"pixi.min.mjs
 #rsync support/pixi.min-v8.1.5.mjs.map "$user"@"$destination_server":"$piefis_support_dir"pixi.min.mjs.map
 #rsync support/pixi-v8.1.5.js "$user"@"$destination_server":"$piefis_support_dir"pixi.js
 #rsync support/pixi-v8.1.5.js.map "$user"@"$destination_server":"$piefis_support_dir"pixi.js.map
 
-rsync "support/Tahoma Bold.ttf" "$user"@"$destination_server":"$piefis_support_dir""Tahoma Bold.ttf"
+rsync support/pixi-v8.6.6.mjs "$user"@"$destination_server":"$piefis_support_dir"pixi.mjs
+rsync support/pixi-v8.6.6.min.mjs "$user"@"$destination_server":"$piefis_support_dir"pixi.min.mjs
+rsync support/pixi-v8.6.6.mjs.map "$user"@"$destination_server":"$piefis_support_dir"pixi.mjs.map
+rsync support/pixi-v8.6.6.min.mjs.map "$user"@"$destination_server":"$piefis_support_dir"pixi.min.mjs.map
+
+rsync "support/Tahoma Bold.ttf" "$user"@"$destination_server":"$piefis_support_dir"Tahoma\ Bold.ttf
 rsync support/Tahoma.ttf "$user"@"$destination_server":"$piefis_support_dir"Tahoma.ttf
-rsync support/efis-test.js "$user"@"$destination_server":"$piefis_support_dir"efis-test.js
+# rsync support/efis-test.js "$user"@"$destination_server":"$piefis_support_dir"efis-test.js
 
 
 
--- Python_Avionics/linux/.bash_profile
+++ Python_Avionics/linux/.bash_profile
@@ -1,3 +1,6 @@
 # ~/.bash_profile
 
+# check that $DISPLAY is empty. If it isn't we are probably already running X
+# check that the virtual terminal number is 1 
+# if both are true then startx
 [[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && startx
